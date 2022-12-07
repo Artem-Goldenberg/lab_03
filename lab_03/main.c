52cb6fda86645423ed689f728a41d3ae959b8530
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2022-12-07 07:36:51 +0300

    Initial Commit

---
+++ lab_03/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab_03
+//
+//  Created by Artem Goldenberg on 07.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
