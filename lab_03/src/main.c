c1051cf0ef6c40ac214f2bf6a55ad3d2ba1d5726
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2022-12-07 11:06:18 +0300

    Sorting is working

--- lab_03/src/main.c
+++ lab_03/src/main.c
@@ -1,9 +1,92 @@
 #include <stdio.h>
 #include "mergesort.h"
 
+#include <string.h>
+#include <stdlib.h>
+
+typedef int (*Comparator)(const void*, const void*);
+typedef void(*Printer)(const void *elem, int last);
+
+// int greater than comparator
+int int_gt_comparator(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
+}
+
+int str_gt_comparator(const void *a, const void *b) {
+    const char **str1 = (const char**)a;
+    const char **str2 = (const char**)b;
+    
+    return strcmp(*str1, *str2);
+}
+
+
+void int_printer(const void *elem, int last) {
+    if (last) printf("%d\n", *(int*)elem);
+    else printf("%d ", *(int*)elem);
+}
+
+void char_printer(const void *elem, int last) {
+    if (last) printf("%c\n", *(int*)elem);
+    else printf("%c ", *(int*)elem);
+}
+
+void str_printer(const void *elem, int last) {
+    const char **str = (const char**)elem;
+    if (last) printf("%s\n", *str);
+    else printf("%s ", *str);
+}
+
+
 int main(int argc, const char * argv[]) {
-    mergesort(NULL, 0, 0, NULL);
-    // insert code here...
-    printf("Hello, World!\n");
+    if (argc <= 2) return 0;
+    
+    Printer printer;
+    Comparator comparator;
+    size_t count = argc - 2;
+    size_t elem_size;
+    void *array;
+    
+    if (strcmp(argv[1], "int") == 0) {
+        printer = int_printer;
+        comparator = int_gt_comparator;
+        elem_size = sizeof(int);
+        
+        array = calloc(count, elem_size);
+        for (int i = 0; i < count; ++i) {
+            int num = atoi(argv[2 + i]);
+            memcpy((char*)array + i * elem_size, &num, elem_size);
+        }
+        
+    } else if (strcmp(argv[1], "char") == 0) {
+        printer = char_printer;
+        comparator = int_gt_comparator;
+        elem_size = sizeof(char);
+        
+        array = calloc(count, elem_size);
+        for (int i = 0; i < count; ++i)
+            memcpy((char*)array + i * elem_size, argv[2 + i], elem_size);
+        
+    } else if (strcmp(argv[1], "str") == 0) {
+        printer = str_printer;
+        comparator = str_gt_comparator;
+        elem_size = sizeof(char*);
+        
+        array = calloc(count, elem_size);
+        for (int i = 0; i < count; ++i)
+            memcpy((char*)array + i * elem_size, argv + 2 + i, elem_size);
+        
+    } else {
+        puts("Undefined behavioud");
+        return 1;
+    }
+    
+    mergesort1(array, count, elem_size, comparator);
+    
+    for (int i = 0; i < count; ++i)
+        printer((char*)array + i * elem_size, i == count - 1);
+    
+    free(array);
+    
+    
     return 0;
 }
